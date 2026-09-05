aba31e9ace878722e019f708604ef9782483e599
leonardo <usantyerleo@gmail.com>
2026-09-05 15:26:54 -0300

    fixing build

--- src/server.ts
+++ src/server.ts
@@ -133,6 +133,6 @@ app.post('/search-loads', async (req: Request, res: Response): Promise<void> => 
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(Number(PORT), '0.0.0.0', () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
